d363ca053b803aa64bb1411a61798c312b2685f2
rachid_bfs <rachidboufous32@gmail.com>
2021-03-21 00:11:58 +0100

    fixing OnlineDbConnection

--- src/main/java/dao/DbConnection.java
+++ src/main/java/dao/DbConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DbConnection {
     private static final String Host = "sql11.freesqldatabase.com";
     private static final int port = 3306;
-    private static final String DB_NAME = "sql11400255";
-    private static final String USERNAME = "sql11400255";
-    private static final String PASSWORD = "3ELHQYcQC6";
+    private static final String DB_NAME = "sql11400341";
+    private static final String USERNAME = "sql11400341";
+    private static final String PASSWORD = "35y1EppCEt";
 
 
     private static Connection connection ;
