985e4c8e969fecd809c4cf99751a982fca810ca8
rachid_bfs <rachidboufous32@gmail.com>
2021-04-16 07:35:38 +0000

    adding base interface (next --> controller)

---
+++ src/main/java/Controller/PwdResetController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021.
+ * programmed by Rachid Boufous.
+ * for FRMFS-ltd organisation
+ *
+ */
+
+package Controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class PwdResetController
+{
+
+    public void Exit_onClick(MouseEvent mouseEvent) {
+
+    }
+}
