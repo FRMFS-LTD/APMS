d7ef88e5cf12385c4fed31ba15e17492609f9018
rachid_bfs <rachidboufous32@gmail.com>
2021-04-18 12:31:19 +0000

    link mainWindow With user Crud + UI colors enhance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/UserCrud.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainWindow.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
