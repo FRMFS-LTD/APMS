f17c50bf4c1dee65de90cf682c4e3a3aa9681d9b
fadwaabdoul <fadwaabdoul@gmail.com>
2021-04-25 03:17:59 +0000

    tryina add the edit and delete icons to parking

--- src/main/java/Controller/ParkingController/Parking.java
+++ src/main/java/Controller/ParkingController/Parking.java
@@ -7,29 +7,46 @@
 
 package Controller.ParkingController;
 
+import Controller.TypetarifController.TypeTarifAdd;
 import Helpers.AppContext;
+import com.github.daytron.simpledialogfx.data.DialogResponse;
+import com.github.daytron.simpledialogfx.dialog.Dialog;
+import com.github.daytron.simpledialogfx.dialog.DialogType;
 import com.jfoenix.controls.JFXButton;
 import dao.Services.parkingService;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import model.Vehicule;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
 import model.parking;
-
+import model.typetarif;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Parking implements Initializable {
@@ -112,4 +129,144 @@ public class Parking implements Initializable {
               refreshDataSet();
     }
 
+
+    public void CreateIcons() {
+
+        Callback<TableColumn<parking, String>, TableCell<parking, String>> cellFactory = (
+                TableColumn<parking, String> param) -> {
+
+            final TableCell<parking, String> cell = new TableCell<parking, String>() {
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+
+                        FontAwesomeIconView DeleteIco = new FontAwesomeIconView(FontAwesomeIcon.TRASH);
+                        FontAwesomeIconView EditIco = new FontAwesomeIconView(FontAwesomeIcon.PENCIL_SQUARE_ALT);
+
+                        StyleIcons(DeleteIco, EditIco);
+
+
+                        DeleteIco.setOnMouseClicked((MouseEvent event) ->
+                        {
+                            DeletetypetarifConfirmation();
+                        });
+
+                        EditIco.setOnMouseClicked((MouseEvent EditEvent) ->
+                        {
+                            parking park = parkingTable.getSelectionModel().getSelectedItem();
+
+                            LoadtypetariIntoUpdateForm(parking);
+
+                        });
+
+
+                        SetIconsToTabViewCell(DeleteIco, EditIco);
+
+                    }
+
+                }
+
+
+                private void LoadparkingIntoUpdateForm(parking park ) {
+                    FXMLLoader loader = new FXMLLoader ();
+                    loader.setLocation(getClass().getResource("/fxml/Typetarifview/Add_typetarif.fxml"));
+                    try {
+                        loader.load();
+                    } catch (IOException ex) {
+                        Logger.getLogger(TypeTarifAdd.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    TypeTarifAdd TypeTarifAdd = loader.getController();
+                    TypeTarifAdd.setUpdate(true);
+                    TypeTarifAdd.initTextFieldForUpdate(TT.getId_typetarif(), TT.getTypetarif(), TT.getPrix());
+
+                    Parent parent = loader.getRoot();
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(parent);
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.UTILITY);
+                    AppContext.UpdateStage(stage,parent,scene);
+                    AppContext.DragScene(stage,parent);
+                    stage.show();
+                }
+
+                private void SetIconsToTabViewCell(FontAwesomeIconView DeleteIco, FontAwesomeIconView EditIco) {
+                    HBox managebtn = new HBox(DeleteIco, EditIco);
+                    managebtn.setStyle("-fx-alignment:center");
+
+                    HBox.setMargin(DeleteIco, new Insets(2, 2, 0, 3));
+                    HBox.setMargin(EditIco, new Insets(2, 3, 0, 2));
+
+                    setGraphic(managebtn);
+                }
+                @FXML
+
+                private void StyleIcons(FontAwesomeIconView DeleteIco, FontAwesomeIconView EditIco) {
+                    DeleteIco.setGlyphSize(26);
+                    DeleteIco.setFill(Color.rgb(251, 62, 56));
+                    DeleteIco.setCursor(Cursor.HAND);
+
+
+                    EditIco.setGlyphSize(26);
+                    EditIco.setFill(Color.rgb(66, 66, 66));
+                    EditIco.setCursor(Cursor.HAND);
+                }
+            };
+            return cell;
+        };
+        OptionCol.setCellFactory(cellFactory);
+    }
+
+    private void DeletetypetarifConfirmation() {
+        typetarif TT = TypeTarfiTable.getSelectionModel().getSelectedItem();
+
+
+        Dialog dialog = new Dialog(
+                DialogType.CONFIRMATION,
+                "Delete User action",
+                "Confirm Action",
+                "Are you sure you want to delete " + TT.getTypetarif() + "?");
+
+        dialog.showAndWait();
+
+        if (dialog.getResponse() == DialogResponse.YES) {
+            TTService.delete(TT.getId_typetarif());
+            refreshDataSet();
+        }
+    }
+
+
+    public void FilterSearch(){
+        FilteredList<typetarif> filteredData = new FilteredList<>(typetarifsList, b -> true);
+        SearchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            filteredData.setPredicate(
+                    typetarif -> {
+                        if(newValue == null || newValue.isEmpty()){
+                            return  true;
+                        }
+                        String lowerCaseFilter = newValue.toLowerCase();
+
+                        if (typetarif.getTypetarif().toLowerCase().indexOf(lowerCaseFilter) != -1 ) {
+                            return true;
+                        }
+
+                        else
+                            return false;
+                    });
+        });
+
+        SortedList<typetarif> sortedData = new SortedList<>(filteredData);
+
+
+        sortedData.comparatorProperty().bind(TypeTarfiTable.comparatorProperty());
+
+        TypeTarfiTable.setItems(sortedData);
+    }
 }
