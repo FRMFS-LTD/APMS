375ce50c3ab772aa1de6512ae5faf4ec244cc4c9
= <rachidboufous32@gmail.com>
2021-05-05 15:19:37 +0000

    linkin the rest of view to main window

--- src/main/java/Controller/MainWindowController.java
+++ src/main/java/Controller/MainWindowController.java
@@ -20,13 +20,35 @@ public class MainWindowController {
 
 
     @FXML
-    private JFXButton AbonnCrudBtn ;
+    private JFXButton statsBtn;
+
+    @FXML
+    private JFXButton AbonnCrudBtn;
+
+    @FXML
+    private JFXButton typeTarifBtn;
+
+    @FXML
+    private JFXButton parkinBtn;
+
+    @FXML
+    private JFXButton HomeBtn;
+
+    @FXML
+    private JFXButton CarsBtn;
+
+    @FXML
+    private JFXButton CustomersBtn;
 
     @FXML
     private JFXButton UsersCrudBtn;
 
     @FXML
+    private JFXButton parkingLots;
+
+    @FXML
     private StackPane mainPane;
+
 
     @FXML
     void UsersCrudBtn_Click(ActionEvent event) {
@@ -39,14 +61,50 @@ public class MainWindowController {
 
 
     public void HandleCLicks(ActionEvent actionEvent) {
+        String ViewPath = "";
         if (actionEvent.getSource() == UsersCrudBtn) {
-            SlideViewToMainWindow("/fxml/UserViews/UserCrud.fxml");
+            ViewPath = "/fxml/UserViews/UserCrud.fxml";
 
         }
 
         if (actionEvent.getSource() == AbonnCrudBtn) {
-            SlideViewToMainWindow("/fxml/AbonnementCrud.fxml");
+            ViewPath = "/fxml/AbonnementCrud.fxml";
         }
+
+       if(actionEvent.getSource() == typeTarifBtn){
+           ViewPath = "/fxml/Typetarifview/TypeTarifView.fxml";
+       }
+
+        if(actionEvent.getSource() == CustomersBtn){
+            ViewPath = "/fxml/ClientViews/ClientCrud.fxml";
+        }
+
+        /*
+        if(actionEvent.getSource() == statsBtn){
+            ViewPath = "/fxml/Typetarifview/TypeTarifView.fxml.fxml";
+        }
+         */
+
+        if(actionEvent.getSource() == parkinBtn){
+            ViewPath = "/fxml/ParkingView/Parking.fxml";
+        }
+
+
+                /*
+        if(actionEvent.getSource() == HomeBtn){
+            //ViewPath = /fxml/ParkingView/Parking.fxml";
+        }
+
+                 */
+
+        if(actionEvent.getSource() == CarsBtn){
+            ViewPath = "/fxml/VehiculeViews/VehiculeCrud.fxml";
+        }
+
+        if(actionEvent.getSource() == parkingLots){
+            ViewPath = "/fxml/StatViews/stationnement.fxml";
+        }
+        SlideViewToMainWindow(ViewPath);
     }
 
     private void SlideViewToMainWindow(String ViewPath) {
